1ecbf0f7a04cf1e9ab3d6316b54362db6b0cf6d5
Stefano M. Nicoletti <stefano@duck.com>
2026-02-26 20:17:09 -0300

    Export Section3_2 and Model3_2 in library root

--- LeanUfo/UFO/UFO.lean
+++ LeanUfo/UFO/UFO.lean
@@ -2,9 +2,15 @@ import LeanUfo.UFO.Modal.Basics
 import LeanUfo.UFO.Modal.FirstOrder
 import LeanUfo.UFO.Modal.Barcan
 
+/- Core Signatures -/
 import LeanUfo.UFO.Core.Signature3_1
 import LeanUfo.UFO.Core.Signature3_2
-import LeanUfo.UFO.Core.Section3_1
 
+/- Axiomatizations -/
+import LeanUfo.UFO.Core.Section3_1
+import LeanUfo.UFO.Core.Section3_2
+
+/- Concrete Models -/
 import LeanUfo.UFO.Models.Model3_1
+import LeanUfo.UFO.Models.Model3_2
 import LeanUfo.UFO.Models.Consistency
